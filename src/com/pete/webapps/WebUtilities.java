8d724eaf25f117dec5797ce34a1cf766286b42fe
Pete <peteski_w@hotmail.com>
2018-07-10 21:47:46 +0100

    fixing error

--- src/com/pete/webapps/WebUtilities.java
+++ src/com/pete/webapps/WebUtilities.java
@@ -3,7 +3,7 @@ package com.pete.webapps;
 public class WebUtilities {
 
 	public String getMyName(){
-		creturn "\n\n\nMy name is jimmy and this is feature 1 branch. im changing. Now a manual change on GIT. And again.";
+		return "\n\n\nMy name is jimmy and this is feature 1 branch. im changing. Now a manual change on GIT. And again.";
 	}
 	
 }
