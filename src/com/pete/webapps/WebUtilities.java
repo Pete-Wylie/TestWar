efb70cf1b1dc0a8285b6dbc9b5dcfda8be204bcb
Pete <peteski_w@hotmail.com>
2018-07-10 21:33:49 +0100

    changing name to jimmy

--- src/com/pete/webapps/WebUtilities.java
+++ src/com/pete/webapps/WebUtilities.java
@@ -3,7 +3,7 @@ package com.pete.webapps;
 public class WebUtilities {
 
 	public String getMyName(){
-		return "\n\n\nMy name is Pete and this is feature 1 branch. im changing. Now a manual change on GIT. And again.";
+		return "\n\n\nMy name is jimmy and this is feature 1 branch. im changing. Now a manual change on GIT. And again.";
 	}
 	
 }
