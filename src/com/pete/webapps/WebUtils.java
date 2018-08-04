34a18e3a07319885276fef541eb8e6d1e74bd9ea
Pete Wylie <36704225+Pete-Wylie@users.noreply.github.com>
2018-08-04 21:39:18 +0100

    ghhg

--- src/com/pete/webapps/WebUtils.java
+++ src/com/pete/webapps/WebUtils.java
@@ -3,7 +3,7 @@ package com.pete.webapps;
 public class WebUtils {
 
 	public String getMyName(){
-		return "\n\n\nMy name is Pete and this is feature 1 branch. im changing. Now a manual change on GIT.";
+		returnz "\n\n\nMy name is Pete and this is feature 1 branch. im changing. Now a manual change on GIT.";
 	}
 	
 }
