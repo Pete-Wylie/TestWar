6350ad675391e821994d9fd44bf206d93e43bbae
Pete Wylie <36704225+Pete-Wylie@users.noreply.github.com>
2018-08-18 19:49:53 +0100

    Update WebUtils.java
    fixing zzz

--- src/com/pete/webapps/WebUtils.java
+++ src/com/pete/webapps/WebUtils.java
@@ -3,7 +3,7 @@ package com.pete.webapps;
 public class WebUtils {
 
 	public String getMyName(){
-		returnzzz "\n\n\nMy name is Pete Wylie and this is feature 1 branch. im changing. Now a manual change on GIT.";
+		return "\n\n\nMy name is Pete Wylie and this is feature 1 branch. im changing. Now a manual change on GIT.";
 	}
 	
 }
