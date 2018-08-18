a7acdebad0992a1f3369ee502d0aef80c575d9a9
Pete <peteski_w@hotmail.com>
2018-08-18 20:52:45 +0100

    dsgfdfgdfg

--- src/com/pete/webapps/WebUtils.java
+++ src/com/pete/webapps/WebUtils.java
@@ -3,6 +3,7 @@ package com.pete.webapps;
 public class WebUtils {
 
 	public String getMyName(){
+		arse
 		return "\n\n\nHi My name is Pete Wylie, hello,  and this is feature 1 branch. im changing. Now a manual change on GIT.";
 	}
 	
